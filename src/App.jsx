6abb0d2640c5eef7109ffe9dcac1d5067f961840
Ahmed Tareq <ahmedtareq404@gmail.com>
2026-05-18 09:20:17 +0300

    Add App component with ScreenshotUploader

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+﻿import React from "react";
+import ScreenshotUploader from "./components/ScreenshotUploader.jsx";
+import { futuristicTheme, toCssVars } from "./theme.js";
+
+export default function App() {
+  return (
+    <main className="app-shell min-h-screen px-6 py-10" style={toCssVars(futuristicTheme)}>
+      <div className="mx-auto max-w-7xl">
+        <ScreenshotUploader />
+      </div>
+    </main>
+  );
+}
